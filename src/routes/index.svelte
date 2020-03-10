68b23540c8c5f51cd237f5d5867729acfc0dbda8
Henry Firth <henrynf12@gmail.com>
2020-03-09 19:24:24 -0500

    Very nice!

---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<style>
+  h1, p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1>Welcome to my blog!</h1>
+<p><strong>I built it with Sapper. Do you like?</strong></p>
